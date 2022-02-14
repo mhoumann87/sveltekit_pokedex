5d8589b911d29566ac10662d084bdb87554c0e40
Michael Houmann <mhoumann87@gmail.com>
2022-02-14 13:09:56 +0100

    Upsate index file :construction:

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	console.log('Hello World');
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>Svelte Kit Pokedex</h1>
