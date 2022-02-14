9897965ba60cb1f62535e0871d5f5ff5017857fd
Michael Houmann <mhoumann87@gmail.com>
2022-02-14 14:39:25 +0100

    Add single 'card' with style and use them in index :sparkles:

---
+++ src/components/pokemanCard.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let info;
+</script>
+
+<a
+	class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
+	href={`/pokeman/${info.id}`}
+>
+	<img src={info.image} alt={info.name} />
+	<h2>{info.id}. {info.name}</h2>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { pokemon } from '../stores/pokestore';
+	import PokemanCard from '../components/pokemanCard.svelte';
 </script>
 
 <svelte:head>
@@ -9,5 +10,5 @@
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
 {#each $pokemon as pokeman}
-	<p>{pokeman.name}</p>
+	<PokemanCard info={pokeman} />
 {/each}
