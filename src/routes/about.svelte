f7e4e9467da48f5029752318c9d529b0a4239617
Michael Houmann <mhoumann87@gmail.com>
2022-02-14 13:13:04 +0100

    Add about file :sparkles:

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Svelte Kit Pokedex - About</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is a really cool Svelte Kit demo that uses the pokemon API to build a Pokedex.</p>
