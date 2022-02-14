f4c29b16f24fa7006bd561d625c86d256ff5e977
Michael Houmann <mhoumann87@gmail.com>
2022-02-14 15:05:47 +0100

    Add function to filter pokemons in index :sparkles:

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,24 @@
 <script>
 	import { pokemon } from '../stores/pokestore';
 	import PokemanCard from '../components/pokemanCard.svelte';
+
+	let searchTerm = '';
+	let filteredPokemon = [];
+
+	$: {
+		//	console.log(searchTerm);
+
+		// if user is scarching, filter the list, else show all pokemons
+		if (searchTerm) {
+			// filter the list
+			filteredPokemon = $pokemon.filter((pokeman) =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		} else {
+			// show all pokemons
+			filteredPokemon = [...$pokemon];
+		}
+	}
 </script>
 
 <svelte:head>
@@ -9,8 +27,15 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	type="text"
+	placeholder="Search Pokemon"
+	bind:value={searchTerm}
+/>
+
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-	{#each $pokemon as pokeman}
+	{#each filteredPokemon as pokeman}
 		<PokemanCard info={pokeman} />
 	{/each}
 </div>
