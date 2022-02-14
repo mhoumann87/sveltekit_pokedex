bb804bbc9319ed192de02e1a9bbd561b00cd5a5a
Michael Houmann <mhoumann87@gmail.com>
2022-02-14 15:52:25 +0100

    Add dynamic page to show pokemon and update project :sparkles:

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -4,7 +4,7 @@
 
 <a
 	class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
-	href={`/pokeman/${info.id}`}
+	href={`/pokemon/${info.id}`}
 >
 	<img class="h-40 w-40" src={info.image} alt={info.name} />
 	<h2 class="text-2xl uppercase">{info.id}. {info.name}</h2>
---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export async function load(url) {
+		const id = url.params.id;
+		const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+		const res = await fetch(apiUrl);
+		const pokeman = await res.json();
+		return { props: { pokeman } };
+	}
+</script>
+
+<script>
+	export let pokeman;
+
+	console.log(pokeman);
+
+	const type = pokeman.types[0].type.name;
+</script>
+
+<div class="flex flex-col items-center">
+	<h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+
+	<p class="text-lg">Type: <strong>{type}</strong></p>
+	<p class="text-lg">Height: <strong>{pokeman.height}</strong></p>
+	<p class="text-lg">Weight: <strong>{pokeman.weight}</strong></p>
+
+	<img class="w-60 h-60 card-image" src={pokeman.sprites['front_default']} alt="pokeman.name" />
+</div>
