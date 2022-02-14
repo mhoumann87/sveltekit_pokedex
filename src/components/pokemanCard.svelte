9f3dfe85bee658a9ef187098d305a513be91ed77
Michael Houmann <mhoumann87@gmail.com>
2022-02-14 14:48:22 +0100

    Update styles for card and add grid to index :lipstick:

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -6,6 +6,6 @@
 	class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
 	href={`/pokeman/${info.id}`}
 >
-	<img src={info.image} alt={info.name} />
-	<h2>{info.id}. {info.name}</h2>
+	<img class="h-40 w-40" src={info.image} alt={info.name} />
+	<h2 class="text-2xl uppercase">{info.id}. {info.name}</h2>
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,8 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
-{#each $pokemon as pokeman}
-	<PokemanCard info={pokeman} />
-{/each}
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each $pokemon as pokeman}
+		<PokemanCard info={pokeman} />
+	{/each}
+</div>
